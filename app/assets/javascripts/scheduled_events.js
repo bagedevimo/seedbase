2848a94b0d5d9f51ba814d57026059a235396492
Matt Fannin <matt.fannin@powershop.co.nz>
2018-02-18 23:34:09 +1300

    Implement real maps.

--- app/assets/javascripts/scheduled_events.js
+++ app/assets/javascripts/scheduled_events.js
@@ -1,2 +1,14 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+function initMap() {
+  var eventMap = document.querySelector('.map');
+  var uluru = { lat: Number(eventMap.dataset.lat), lng: Number(eventMap.dataset.long) };
+  var map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 15,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
