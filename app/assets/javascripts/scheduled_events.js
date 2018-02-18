cad55430e0e56b6d752244e64700a48727f25d99
Matt Fannin <matt.fannin@powershop.co.nz>
2018-02-19 00:03:11 +1300

    Create a simplified map theme to make it more readible at it's small size.

--- app/assets/javascripts/scheduled_events.js
+++ app/assets/javascripts/scheduled_events.js
@@ -1,14 +1,63 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 function initMap() {
+  var styledMapType = new google.maps.StyledMapType(
+    [
+      {
+        "featureType": "poi",
+        "elementType": "labels.text",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "poi.business",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "road",
+        "elementType": "labels.icon",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      },
+      {
+        "featureType": "transit",
+        "stylers": [
+          {
+            "visibility": "off"
+          }
+        ]
+      }
+    ],
+    {name: 'Simple'}
+  );
+
   var eventMap = document.querySelector('.map');
-  var uluru = { lat: Number(eventMap.dataset.lat), lng: Number(eventMap.dataset.long) };
+  var uluru = {lat: Number(eventMap.dataset.lat), lng: Number(eventMap.dataset.long)};
+
   var map = new google.maps.Map(document.querySelector('.map'), {
-    zoom: 15,
-    center: uluru
+   center: uluru,
+   zoom: 15,
+   mapTypeControlOptions: {
+     mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain',
+             'styled_map']
+   }
   });
+
   var marker = new google.maps.Marker({
-    position: uluru,
-    map: map
+   position: uluru,
+   map: map
   });
+
+  map.mapTypes.set('styled_map', styledMapType);
+  map.setMapTypeId('styled_map');
 }
