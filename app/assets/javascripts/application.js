a111ac89e2c8eac79dee96e1d71a4a7febbcc8b8
Matt Fannin <matt.fannin@powershop.co.nz>
2018-03-11 23:19:13 +1300

    Move dropdown js to it's own file.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,15 +13,3 @@
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
-
-if (document.querySelector('.flyout')) {
-  var flyout = document.querySelector('.flyout');
-
-  document.addEventListener('click', function(e) {
-    if (e.target == flyout) {
-      flyout.classList.toggle('active');
-    } else if (flyout.classList.contains('active')) {
-      flyout.classList.remove('active');
-    }
-  });
-}
---
+++ app/assets/javascripts/modules/_flyout.js
@@ -0,0 +1,13 @@
+// Toggles visability of the profile dropdown if present
+
+if (document.querySelector('.flyout')) {
+  var flyout = document.querySelector('.flyout');
+
+  document.addEventListener('click', function(e) {
+    if (e.target == flyout) {
+      flyout.classList.toggle('active');
+    } else if (flyout.classList.contains('active')) {
+      flyout.classList.remove('active');
+    }
+  });
+}
